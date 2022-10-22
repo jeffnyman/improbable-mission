db23ee847aa51fed8f7ebef7bfdaf63f286b131d
Jeff Nyman <jeffnyman@gmail.com>
2022-10-22 03:41:58 -0500

    Checking for browser base capabilities.

--- src/assets/js/engine.js
+++ src/assets/js/engine.js
@@ -1,6 +1,46 @@
 function Engine() {
   this.init = function () {
     log("Engine Started ");
+
+    // If there is a query portion to the URL, add the current
+    // index to the browser's session history stack.
+
+    if (document.location.search != "") {
+      window.history.pushState(false, false, "/");
+    }
+
+    engine.checkCapabilities(
+      function () {
+        log("Check capabilities: SUCCESS");
+      },
+      function () {
+        // things went south
+      },
+    );
+  };
+
+  this.checkCapabilities = function (success, fail) {
+    log("Checking capabilities ...");
+
+    var allGood = true;
+
+    // Check if canvas creation is possible.
+    var testCanvas = document.createElement("canvas");
+
+    if (!(testCanvas.getContext && testCanvas.getContext("2d"))) {
+      allGood = false;
+    }
+
+    // Check if requestAnimationFrame is available.
+    if (!window.requestAnimationFrame) {
+      allGood = false;
+    }
+
+    if (allGood) {
+      success();
+    } else {
+      fail();
+    }
   };
 }
 
