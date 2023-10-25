eab8e012a7b935c91ee7ceac924a2c42681eef68
Jeff Nyman <jeffnyman@gmail.com>
2023-10-25 01:59:51 -0500

    Add a coordinator module.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,9 +1,13 @@
 import { checkBrowserCapabilities } from "./utils/browserCheck";
+import { Coordinator } from "./coordinator";
 
 export class App {
   constructor() {
     console.log("App Constructor");
 
     checkBrowserCapabilities();
+
+    const coordinator = new Coordinator();
+    coordinator.start();
   }
 }
---
+++ src/scripts/coordinator.ts
@@ -0,0 +1,9 @@
+export class Coordinator {
+  constructor() {
+    console.log("Coordinator Constructed");
+  }
+
+  start() {
+    console.log("|| Coordinator Started ||");
+  }
+}
