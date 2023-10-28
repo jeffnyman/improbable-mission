98f909f50f912500fec4a8709e08798c09014d90
Jeff Nyman <jeffnyman@gmail.com>
2023-10-28 05:10:56 -0500

    Add ability to load a sound resource.

--- src/scripts/utils/resourceLoader.ts
+++ src/scripts/utils/resourceLoader.ts
@@ -78,8 +78,49 @@ export class ResourceLoader {
   loadSounds() {
     console.log("** Loading Sounds");
 
+    if (!this.audio.context) {
+      return;
+    }
+
     return new Promise<void>((resolve, reject) => {
-      resolve();
+      var resourceFileName = this.audioResources[0];
+      var resourceName = resourceFileName.split(".")[0];
+
+      console.log(resourceName);
+
+      // Initiate an asynchronous request to fetch an audio resource.
+      // The response will contain the audio data in the form of an
+      // array buffer that can be processed further.
+
+      var request = new XMLHttpRequest();
+      request.open("GET", "../sounds/" + resourceFileName, true);
+      request.responseType = "arraybuffer";
+
+      // Assuming a resource was loaded, that resource is decoded and
+      // the decoded data is stored. The logic keeps track of how many
+      // resources have been loaded.
+
+      request.onload = () => {
+        if (request.status === 200 && request.response) {
+          console.log("Sound resource found.");
+          resolve();
+        } else {
+          console.log(
+            `Unable to find sound resource. URL: ${request.responseURL}`,
+          );
+          reject(
+            new Error(
+              `ERROR: Cannot Load Mission Audio: ${request.responseURL}`,
+            ),
+          );
+        }
+      };
+
+      // This statement is crucial! This is what actually sends the
+      // XMLHttpRequest and fetches the audio resource. This should
+      // trigger the onload event, which then starts the process of
+      // decoding and loading the resource.
+      request.send();
     });
   }
 
@@ -100,7 +141,7 @@ export class ResourceLoader {
   }
 
   gatherAudioResources() {
-    this.audioResources.push("1");
+    this.audioResources.push("anotherVisitor.ogg");
 
     this.neededResources += this.audioResources.length;
     this.updateNeededResources();
