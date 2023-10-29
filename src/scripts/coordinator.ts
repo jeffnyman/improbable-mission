843c830601c8f2b0f54d2eef1eff525be5a3c50c
Jeff Nyman <jeffnyman@gmail.com>
2023-10-29 08:08:23 -0500

    Add a layout module.

--- src/scripts/coordinator.ts
+++ src/scripts/coordinator.ts
@@ -1,8 +1,13 @@
 import { ResourceLoader } from "./utils/resourceLoader";
+import { Layout } from "./layout";
 
 export class Coordinator {
+  layout: Layout;
+
   constructor() {
     console.log("Coordinator Constructed");
+
+    this.layout = new Layout();
   }
 
   start() {
