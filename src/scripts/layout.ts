0f30cc704e07c0481284e8bfe25f37f95ccdf627
Jeff Nyman <jeffnyman@gmail.com>
2023-10-31 09:30:11 -0500

    Add a data structure for maps.

--- src/scripts/layout.ts
+++ src/scripts/layout.ts
@@ -1,6 +1,14 @@
+import { maps } from "./data/maps";
+
 export class Layout {
+  map: { rooms: { 0: number[] } };
+
   constructor() {
     console.log("Layout Constructed");
+
+    this.map = maps[0];
+
+    console.log(`MAP: ${JSON.stringify(this.map.rooms)}`); // DEBUG
   }
 
   generate() {
