90a35982845e7fa4fc40dc37b2f89b7f4aaa8ad3
Jeff Nyman <jeffnyman@gmail.com>
2024-05-05 11:41:52 -0500

    Add core application element.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -10,4 +10,14 @@ acceptMissionSpan.addEventListener("click", () => missionAccepted());
 
 function missionAccepted() {
   console.log("Mission has been accepted.");
+
+  const missionDiv = document.querySelector<HTMLDivElement>("#mission")!;
+  missionDiv.remove();
+
+  const appDiv = document.querySelector<HTMLDivElement>("#app")!;
+  appDiv.classList.remove("hidden");
+
+  appDiv.innerHTML = `
+  <p>Improbable Mission</p>
+`;
 }
