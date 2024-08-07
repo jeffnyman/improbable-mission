68524e20debdb4ce8b74c1554bfcb465bf91bacc
Jeff Nyman <jeffnyman@gmail.com>
2024-08-07 06:14:27 -0500

    Add initial script logic.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,15 @@
 import "../styles/mission.css";
 
 let game: string = "Improbable Mission";
-console.log(game);
+
+console.log(`... ${game} ...`);
+console.log("Browser ready; mission provided.");
+
+const acceptMissionSpan =
+  document.querySelector<HTMLSpanElement>("#accept-mission");
+
+acceptMissionSpan.addEventListener("click", () => missionAccepted());
+
+function missionAccepted() {
+  console.log("Mission has been accepted.");
+}
