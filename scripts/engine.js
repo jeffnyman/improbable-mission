f80d8a54a73abcee4a1ed127c3a8d62600984f08
Jeff Nyman <jeffnyman@gmail.com>
2025-09-19 08:03:47 -0500

    Add error handling to engine initialization.

--- scripts/engine.js
+++ scripts/engine.js
@@ -5,5 +5,15 @@ export class Engine {
 
   init() {
     console.log("Engine Initialized");
+
+    try {
+      // Redirect to the landing page if the URL has a query string.
+      if (document.location.search !== "") {
+        window.history.pushState(false, false, "/");
+      }
+    } catch (error) {
+      console.error("Initialization failed:", error);
+      return;
+    }
   }
 }
