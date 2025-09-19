4d3602c6a399d965e5b31681ea91cefb8713c021
Jeff Nyman <jeffnyman@gmail.com>
2025-09-19 10:04:08 -0500

    Add browser functionality check.

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,16 +1,18 @@
+import { checkBrowserCapabilities } from "./browserCheck";
+
 export class Engine {
   constructor() {
     console.log("Engine Constructed");
   }
 
   init() {
-    console.log("Engine Initialized");
-
     try {
       // Redirect to the landing page if the URL has a query string.
       if (document.location.search !== "") {
         window.history.pushState(false, false, "/");
       }
+
+      checkBrowserCapabilities();
     } catch (error) {
       console.error("Initialization failed:", error);
       return;
