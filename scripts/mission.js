de898df5f723ab872fb83bfe42417c7de1f67294
Jeff Nyman <jeffnyman@gmail.com>
2025-09-24 15:20:11 -0500

    Add landing page.

--- scripts/mission.js
+++ scripts/mission.js
@@ -4,3 +4,31 @@ console.log(`... ${game} ...`);
 console.log("Browser ready; mission provided.");
 
 document.documentElement.classList.replace("no-js", "js");
+
+const acceptMission = document.getElementById("accept-mission");
+
+if (acceptMission) {
+  acceptMission.addEventListener("click", () => missionAccepted());
+} else {
+  console.error("Element with id 'accept-mission' not found.");
+}
+
+function missionAccepted() {
+  console.log("Mission has been accepted.");
+
+  const missionDiv = document.getElementById("mission");
+
+  if (missionDiv) {
+    missionDiv.remove();
+  } else {
+    console.error("Element with id 'mission' not found.");
+  }
+
+  const appDiv = document.getElementById("app");
+
+  if (appDiv) {
+    appDiv.classList.remove("hidden");
+  } else {
+    console.error("Element with id 'app' not found.");
+  }
+}
