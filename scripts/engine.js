e37202e010a3517606853db8ab2e1bbafc55b427
Jeff Nyman <jeffnyman@gmail.com>
2025-10-01 03:54:22 -0500

    Add handling for closing dossier.

--- scripts/engine.js
+++ scripts/engine.js
@@ -6,15 +6,32 @@ export class Engine {
 
     this.setupInterface();
     this.setupToolbar();
+    this.setupKeyHandling();
   }
 
   setupInterface() {
     document.getElementById("toolbar").classList.remove("hidden");
+
+    document.getElementById("overlay").addEventListener("click", () => {
+      document.getElementById("overlay").classList.add("hidden");
+      document.getElementById("dossier").classList.add("hidden");
+    });
   }
 
   setupToolbar() {
     document.getElementById("dossier-button").addEventListener("click", () => {
+      document.getElementById("overlay").classList.remove("hidden");
       document.getElementById("dossier").classList.remove("hidden");
+    });
+  }
+
+  setupKeyHandling() {
+    document.addEventListener("keydown", (evt) => {
+      var overlay = document.getElementById("overlay");
+
+      if (evt.code === "Escape" && !overlay.classList.contains("hidden")) {
+        overlay.click();
+      }
     });
   }
 }
