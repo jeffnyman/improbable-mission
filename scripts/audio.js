2acd580ff1dd1aad2b5d8a2895295bb676bc4c51
Jeff Nyman <jeffnyman@gmail.com>
2025-10-01 15:12:46 -0500

    Add ability to load sound files.

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,6 +1,41 @@
 export class GameAudio {
   constructor() {
     this.context = null;
+    this.sounds = {};
+    this.resources = [
+      "anotherVisitor.ogg",
+      "elevatorStart.ogg",
+      "elevatorStop.ogg",
+      "stepLeft.ogg",
+      "stepRight.ogg",
+      "jumpLeft.ogg",
+      "jumpRight.ogg",
+      "droid.ogg",
+      "droidTurn.ogg",
+      "inLine.ogg",
+      "falling.ogg",
+      "dieByZap.ogg",
+      "destroyHim.ogg",
+      "hahaha.ogg",
+      "nonono.ogg",
+      "missionAccomplished.ogg",
+    ];
+
+    for (let i = 1; i <= 2; i++) {
+      this.resources.push(`dial${i}.ogg`);
+    }
+
+    for (let i = 1; i <= 5; i++) {
+      this.resources.push(`beep${i}.ogg`);
+    }
+
+    for (let i = 1; i <= 5; i++) {
+      this.resources.push(`zap${i}.ogg`);
+    }
+
+    for (let i = 1; i <= 14; i++) {
+      this.resources.push(`organTone${i}.ogg`);
+    }
   }
 
   init() {
--- scripts/loadResources.js
+++ scripts/loadResources.js
@@ -4,10 +4,17 @@ export async function loadResources(engine) {
     loaded: 0,
   };
 
+  if (engine.audio.context) {
+    loadingState.required += engine.audio.resources.length;
+  }
+
   document.getElementById("loading").classList.remove("hidden");
   document.getElementById("required").textContent = loadingState.required;
 
-  await loadSpriteResources(engine, loadingState);
+  await Promise.all([
+    loadSpriteResources(engine, loadingState),
+    loadAudioResources(engine, loadingState),
+  ]);
 }
 
 function loadSpriteResources(engine, loadingState) {
@@ -28,4 +35,37 @@ function loadSpriteResources(engine, loadingState) {
 
     gameSprites.src = `${import.meta.env.BASE_URL}images/missionSprites.png`;
   });
+}
+
+async function loadAudioResources(engine, loadingState) {
+  if (!engine.audio.context) {
+    console.warn("Audio context not available.");
+    return Promise.resolve();
+  }
+
+  for (const audioFile of engine.audio.resources) {
+    await loadAudio(audioFile, engine, loadingState);
+  }
+}
+
+async function loadAudio(audioFile, engine, loadingState) {
+  const name = audioFile.split(".")[0];
+  const response = await fetch(`${import.meta.env.BASE_URL}audio/${audioFile}`);
+  const arrayBuffer = await response.arrayBuffer();
+  let buffer = null;
+
+  try {
+    buffer = await engine.audio.context.decodeAudioData(arrayBuffer);
+  } catch (error) {
+    throw new Error(`${audioFile}: ${error.message}.`);
+  }
+
+  loadingState.loaded++;
+  document.getElementById("loaded").textContent = loadingState.loaded;
+
+  engine.audio.sounds[name] = buffer;
+
+  // This is a random delay to simulate the retro-feel.
+  const delay = Math.random() * 100 + 50; // 50-150ms
+  await new Promise((resolve) => setTimeout(resolve, delay));
 }
