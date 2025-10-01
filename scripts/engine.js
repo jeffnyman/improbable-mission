fc2285c4a3cb6e40a9d03df560c7230fd28126a7
Jeff Nyman <jeffnyman@gmail.com>
2025-10-01 03:34:54 -0500

    Add initial interface setup.

--- scripts/engine.js
+++ scripts/engine.js
@@ -3,5 +3,11 @@ import { checkBrowser } from "./checkBrowser";
 export class Engine {
   init() {
     checkBrowser();
+
+    this.setupInterface();
+  }
+
+  setupInterface() {
+    document.getElementById("toolbar").classList.remove("hidden");
   }
 }
