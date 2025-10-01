25de28f8545580d71a79d8162636bc3866da496b
Jeff Nyman <jeffnyman@gmail.com>
2025-10-01 03:05:29 -0500

    Add logic to accept mission.

--- scripts/mission.js
+++ scripts/mission.js
@@ -4,3 +4,13 @@ console.log(`... ${game} ...`);
 console.log("Browser ready; mission provided.");
 
 document.documentElement.classList.replace("no-js", "js");
+
+document
+  .getElementById("accept")
+  .addEventListener("click", () => missionAccepted());
+
+function missionAccepted() {
+  console.log("Mission has been accepted.");
+
+  document.getElementById("mission").remove();
+}
