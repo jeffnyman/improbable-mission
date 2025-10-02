17dc5a2657f703302c2b43d034bec34602c2c07e
Jeff Nyman <jeffnyman@gmail.com>
2025-10-02 03:29:11 -0500

    Add sprite color swapping.

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,11 +1,13 @@
 import { checkBrowser } from "./checkBrowser";
 import { loadResources } from "./loadResources";
 import { GameAudio } from "./audio";
+import { palette } from "./palette";
 
 export class Engine {
   constructor() {
     // Sprite management
     this.gameSprites = null;
+    this.sprites = {};
     this.spriteWidth = 800;
     this.spriteHeight = 600;
     this.baseSpriteCanvas = null;
@@ -51,6 +53,12 @@ export class Engine {
       this.spriteWidth,
       this.spriteHeight,
     );
+
+    Object.entries(palette).forEach(([name, colors]) => {
+      if (name !== "source") {
+        this.swapSpritePalette(name, colors);
+      }
+    });
   }
 
   setupInterface() {
@@ -78,5 +86,64 @@ export class Engine {
         overlay.click();
       }
     });
+  }
+
+  swapSpritePalette(name, targetPalette) {
+    var newImageData = this.outputSpriteData;
+
+    for (
+      var pixelIndex = 0, totalPixels = this.baseSpritePixels.length;
+      pixelIndex < totalPixels;
+      pixelIndex += 4
+    ) {
+      var currentRed = this.baseSpritePixels[pixelIndex];
+      var currentGreen = this.baseSpritePixels[pixelIndex + 1];
+      var currentBlue = this.baseSpritePixels[pixelIndex + 2];
+
+      for (var colorIndex = 0; colorIndex < 16; colorIndex++) {
+        var sourceColor = palette.source[colorIndex];
+        var sourceRed = parseInt(sourceColor[0] + sourceColor[1], 16);
+        var sourceGreen = parseInt(sourceColor[2] + sourceColor[3], 16);
+        var sourceBlue = parseInt(sourceColor[4] + sourceColor[5], 16);
+
+        if (
+          currentRed == sourceRed &&
+          currentGreen == sourceGreen &&
+          currentBlue == sourceBlue
+        ) {
+          var targetColor = targetPalette[colorIndex];
+
+          newImageData.data[pixelIndex] = parseInt(
+            targetColor[0] + targetColor[1],
+            16,
+          );
+
+          newImageData.data[pixelIndex + 1] = parseInt(
+            targetColor[2] + targetColor[3],
+            16,
+          );
+
+          newImageData.data[pixelIndex + 2] = parseInt(
+            targetColor[4] + targetColor[5],
+            16,
+          );
+
+          newImageData.data[pixelIndex + 3] =
+            this.baseSpritePixels[pixelIndex + 3];
+
+          break;
+        }
+      }
+    }
+
+    var outputCanvas = document.createElement("canvas");
+    outputCanvas.width = this.spriteWidth;
+    outputCanvas.height = this.spriteHeight;
+
+    var outputContext = outputCanvas.getContext("2d");
+    outputContext.putImageData(newImageData, 0, 0);
+
+    this.sprites[name] = new Image();
+    this.sprites[name].src = outputCanvas.toDataURL("image/png");
   }
 }
