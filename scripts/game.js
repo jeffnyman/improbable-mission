320adb8604d3aa82fad61371504a9644b605c0de
Jeff Nyman <jeffnyman@gmail.com>
2025-10-03 07:23:59 -0500

    Add ability to get a layout of rooms.

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,13 @@
+import { layouts } from "./layout";
+
 export class Game {
+  constructor() {
+    this.layoutId = 0;
+    this.layout = layouts[this.layoutId].rooms;
+
+    console.log(`Layout ID: ${this.layoutId}`); // REMOVE
+    console.log(`Layout: ${JSON.stringify(this.layout)}`); // REMOVE
+  }
+
   init() {}
 }
