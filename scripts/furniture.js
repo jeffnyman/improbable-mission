ca56898762667a5c1399c4ac208a778f8b92ca4d
Jeff Nyman <jeffnyman@gmail.com>
2025-10-05 09:53:16 -0500

    Add furniture properties.

--- scripts/furniture.js
+++ scripts/furniture.js
@@ -1,10 +1,16 @@
+import { furnitureProperties } from "./layout";
+
 export class Furniture {
   constructor(roomId, type, left, bottom) {
     this.roomId = roomId;
     this.type = type;
     this.left = left;
     this.bottom = bottom;
+    this.searchTime = null;
   }
 
-  init() {}
+  init() {
+    var fp = furnitureProperties[this.type];
+    this.searchTime = fp.s;
+  }
 }
