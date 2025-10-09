c6119c155f9cbf0f810c7c77c75e2e7dc3ef1b3f
Jeff Nyman <jeffnyman@gmail.com>
2025-10-09 08:58:14 -0500

    Add borders for elevator shaft.

--- scripts/elevator.js
+++ scripts/elevator.js
@@ -5,10 +5,24 @@ export class Elevator {
     this.utils = utils;
   }
 
-  init() {}
+  init() {
+    // Elevator vertical top position.
+    this.y = 0;
+  }
 
   animationRoutine() {
     this.utils.rect(128, 0, 64, 200, 0);
     this.utils.draw(708, 16, 48, 96, 136, 0);
+
+    // Borders
+    for (var i = 0; i < 10; i++) {
+      let borderOffset = this.y % 24;
+
+      // Left border.
+      this.utils.draw(756, 16, 8, 24, 128, i * 24 - 16 - borderOffset);
+
+      // Right border.
+      this.utils.draw(756, 16, 8, 24, 184, i * 24 - 16 - borderOffset);
+    }
   }
 }
