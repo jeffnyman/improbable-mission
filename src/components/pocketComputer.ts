d14967d5cadc225c01d827a14e678a1b31b9a119
Jeff Nyman <jeffnyman@gmail.com>
2025-11-06 09:12:12 -0600

    Update display of pocket computer.

--- src/components/pocketComputer.ts
+++ src/components/pocketComputer.ts
@@ -1,8 +1,19 @@
 import { graphics } from "../utils/graphics";
 
 export class PocketComputer {
+  // The pocket computer has three states: map, puzzles, phone.
+  private state = "map";
+
   animationRoutine() {
     // Draw the full interface.
     graphics.draw(0, 200, 320, 80, 0, 120);
+
+    if (this.state === "map") {
+      // Draw the map border.
+      graphics.draw(0, 280, 164, 59, 58, 131);
+
+      // Establish the map view boundary.
+      graphics.rect(64, 136, 152, 48, 5);
+    }
   }
 }
