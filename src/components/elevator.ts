ca42b7063a4280e9ec27ca09a51814d24122d239
Jeff Nyman <jeffnyman@gmail.com>
2025-11-26 09:03:49 -0600

    chore: add borders to elevator shaft

--- src/components/elevator.ts
+++ src/components/elevator.ts
@@ -1,11 +1,27 @@
 import { graphics } from "../utils/graphics";
 
 export class Elevator {
+  // Elevator vertical top position. This is the scroll position
+  // of the world. The borders, walls, and corridors will all be
+  // drawn with offsets calculated from this value.
+  private y = 0;
+
   animationRoutine() {
     // Draw the elevator shaft.
     graphics.rect(128, 0, 64, 200);
 
     // Draw the elevator.
     graphics.draw(708, 16, 48, 96, 136, 0);
+
+    // Draw the borders.
+    for (let i = 0; i < 10; i++) {
+      const borderOffset = this.y % 24;
+
+      // Left border.
+      graphics.draw(756, 16, 8, 24, 128, i * 24 - 16 - borderOffset);
+
+      // Right border.
+      graphics.draw(756, 16, 8, 24, 184, i * 24 - 16 - borderOffset);
+    }
   }
 }
