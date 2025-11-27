9459261e0d208a4556eac9fb3ab75b9d7da8be5b
Jeff Nyman <jeffnyman@gmail.com>
2025-11-27 14:47:01 -0600

    chore: add keyboard manager

---
+++ src/utils/keyboard.ts
@@ -0,0 +1,28 @@
+export class KeyboardManager {
+  private pressedKeys: Record<string, boolean> = {};
+
+  public keys = {};
+
+  constructor() {
+    document.addEventListener("keydown", (e) => this.handleKeyDown(e));
+    document.addEventListener("keyup", (e) => this.handleKeyUp(e));
+  }
+
+  isKeyPressed(code: string) {
+    return this.pressedKeys[code] === true;
+  }
+
+  handleKeyDown(e: KeyboardEvent) {
+    if (Object.values(this.keys).includes(e.code)) {
+      e.preventDefault();
+    }
+
+    this.pressedKeys[e.code] = true;
+  }
+
+  handleKeyUp(e: KeyboardEvent) {
+    this.pressedKeys[e.code] = false;
+  }
+}
+
+export const keyboard: KeyboardManager = new KeyboardManager();
