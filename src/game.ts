162c378333e357709f0586f2ca9411158726d5be
Jeff Nyman <jeffnyman@gmail.com>
2025-11-28 08:54:32 -0600

    chore: cleanup logging code

--- src/game.ts
+++ src/game.ts
@@ -1,15 +1,12 @@
 import { sceneManager } from "./components/sceneManager";
 import { Elevator } from "./components/elevator";
 import { PocketComputer } from "./components/pocketComputer";
-import { logOnce } from "./utils/logger";
 
 export class Game {
   private elevator: Elevator = new Elevator();
   private pocketComputer: PocketComputer = new PocketComputer();
 
   updateScan() {
-    logOnce("Scan routine on game...");
-
     if (sceneManager.getScene() === "elevator") {
       this.elevator.scanRoutine();
     }
