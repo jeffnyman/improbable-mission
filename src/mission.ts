6fdb32540cacccefc3d202a0a50772d470134618
Jeff Nyman <jeffnyman@gmail.com>
2025-12-13 11:10:30 -0600

    refactor: avoid flash of unstyled content on startup

--- src/mission.ts
+++ src/mission.ts
@@ -2,8 +2,6 @@ if (import.meta.env.DEV) {
   console.log(import.meta.env.VITE_APP_TITLE);
 }
 
-document.documentElement.classList.replace("no-js", "js");
-
 if (document.location.search) {
   window.history.pushState({}, "", "/");
 }
