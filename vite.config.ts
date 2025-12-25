a2e05b3b944ca705da133d70760405fbbe31b4f8
Jeff Nyman <jeffnyman@gmail.com>
2025-12-25 07:31:30 -0600

    Update build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
   base: "/improbable-mission/",
+
+  plugins: [
+    checker({
+      typescript: true,
+
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/**/*.ts"',
+      },
+
+      stylelint: {
+        lintCommand: 'stylelint "styles/**/*.css"',
+      },
+    }),
+  ],
 });
