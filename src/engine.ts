d88d041c80b713b6b4cc6c98abf966225ab62cdc
Jeff Nyman <jeffnyman@gmail.com>
2026-01-15 11:42:51 -0600

    chore: add animation loop

--- src/engine.ts
+++ src/engine.ts
@@ -1,15 +1,36 @@
 import { browser } from "./utils/browser";
 import { graphics } from "./utils/graphics";
+import { log } from "./utils/logger";
 import { canvasResizer } from "./ui/canvasResizer";
 import { sprites } from "./utils/sprites";
 
 class Engine {
+  private animationFrameTime = 0;
+  private readonly FRAME_INTERVAL = 30;
+
   async init() {
     browser.requireElement("app").classList.remove("hidden");
 
     await sprites.loadSprites();
     graphics.init("game");
     canvasResizer.init();
+
+    this.animate();
+  }
+
+  private animationRoutine() {
+    log("In animation loop ..."); // TEMPORARY
+  }
+
+  private animate() {
+    requestAnimationFrame((actualTime) => {
+      if (actualTime - this.animationFrameTime > this.FRAME_INTERVAL) {
+        this.animationFrameTime = actualTime;
+        this.animationRoutine();
+      }
+
+      this.animate();
+    });
   }
 }
 
