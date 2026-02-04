4cb66c8ee0a16817922dae9c7f48f7f64a11dbae
Jeff Nyman <jeffnyman@gmail.com>
2026-02-04 08:15:33 -0600

    chore: draw elevator shaft borders

--- src/component/elevator.ts
+++ src/component/elevator.ts
@@ -1,12 +1,25 @@
 import { graphics } from "../utils/graphics";
 
 class Elevator {
+  private y = 0;
+
   animationRoutine() {
     // Draw the elevator shaft.
     graphics.rect(128, 0, 64, 200, 0);
 
     // Render the elevator sprite.
     graphics.draw(708, 16, 48, 96, 136, 0);
+
+    // Draw the elevator borders.
+    for (let i = 0; i < 10; i++) {
+      const borderOffset = this.y % 24;
+
+      // Left border.
+      graphics.draw(756, 16, 8, 24, 128, i * 24 - 16 - borderOffset);
+
+      // Right border.
+      graphics.draw(756, 16, 8, 24, 184, i * 24 - 16 - borderOffset);
+    }
   }
 }
 
