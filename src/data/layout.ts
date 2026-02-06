1e0ac2b0ef979df62da77abb8155b9546741d6d2
Jeff Nyman <jeffnyman@gmail.com>
2026-02-06 02:21:07 -0600

    chore: add data structure for room doors

--- src/data/layout.ts
+++ src/data/layout.ts
@@ -1,3 +1,8 @@
+/**
+ * A layout is a map made up of rooms. There are 54 possible
+ * locations and 32 possible rooms within those locations. A
+ * value of 0 indicates no room.
+ */
 export const maps = [
   {
     rooms: {
@@ -12,4 +17,49 @@ export const maps = [
       8: [0, 0, 0, 0, 0, 31],
     },
   },
+];
+
+/**
+ * Left doors: types 1 (top-left) or 4 (bottom-left)
+ * Right doors: types 2 (top-right) or 3 (bottom-right)
+ *
+ * 1 = Top-left corner exit
+ * 2 = Top-right corner exit
+ * 3 = Bottom-right corner exit
+ * 4 = Bottom-left corner exit
+ */
+export const roomDoors = [
+  [0],
+  [3],
+  [2, 4],
+  [1, 3],
+  [1, 2],
+  [2, 4],
+  [1],
+  [2, 4],
+  [1, 2],
+  [2, 4],
+  [2],
+  [3],
+  [4],
+  [1, 3],
+  [3, 4],
+  [1, 2],
+  [3, 4],
+  [1, 3],
+  [2],
+  [4],
+  [1, 2],
+  [1],
+  [1, 3],
+  [1],
+  [3, 4],
+  [4],
+  [1],
+  [3],
+  [2],
+  [3, 4],
+  [2],
+  [4],
+  [3],
 ];
