85b8dd73ba070ca3d1e16a183aad89bd09187e65
Jeff Nyman <jeffnyman@gmail.com>
2026-02-07 04:00:46 -0600

    chore: add initial map view to pocket computer

--- src/component/pocketComputer.ts
+++ src/component/pocketComputer.ts
@@ -4,6 +4,12 @@ class PocketComputer {
   animationRoutine() {
     // Draw the full computer interface at the bottom of the screen.
     graphics.draw(0, 200, 320, 80, 0, 120);
+
+    // Draw the map border.
+    graphics.draw(0, 280, 164, 59, 58, 131);
+
+    // Establish the map view boundary.
+    graphics.rect(64, 136, 152, 48, 5);
   }
 }
 
