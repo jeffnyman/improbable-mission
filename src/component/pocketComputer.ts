40f05811604f20bed40d3799f2c0edd7ec09ad72
Jeff Nyman <jeffnyman@gmail.com>
2026-02-07 05:22:23 -0600

    chore: add state property to pocket computer

--- src/component/pocketComputer.ts
+++ src/component/pocketComputer.ts
@@ -12,6 +12,8 @@ class PocketComputer {
   // format of layout
   private revealedMap: number[][] = [];
 
+  private state = "map";
+
   constructor() {
     // Initialize 6 levels, each with 9 elevator positions (0-8).
     // Note that there are only 8 distinct elevators.
@@ -28,33 +30,37 @@ class PocketComputer {
   }
 
   animationRoutine() {
-    // Draw the full computer interface at the bottom of the screen.
-    graphics.draw(0, 200, 320, 80, 0, 120);
+    if (this.state === "map") {
+      // Draw the full computer interface at the bottom of the screen.
+      graphics.draw(0, 200, 320, 80, 0, 120);
 
-    // Draw the map border.
-    graphics.draw(0, 280, 164, 59, 58, 131);
+      // Draw the map border.
+      graphics.draw(0, 280, 164, 59, 58, 131);
 
-    // Establish the map view boundary.
-    graphics.rect(64, 136, 152, 48, 5);
+      // Establish the map view boundary.
+      graphics.rect(64, 136, 152, 48, 5);
 
-    for (let elevator = 1; elevator <= 9; elevator++) {
-      for (let level = 0; level < 6; level++) {
-        // Only draw the elevator shaft line if this section has
-        // been revealed.
-        if (this.revealedMap[level][elevator - 1] === 1) {
-          graphics.rect(83 + (elevator - 1) * 16, 136 + level * 8, 2, 8, 0);
+      for (let elevator = 1; elevator <= 9; elevator++) {
+        for (let level = 0; level < 6; level++) {
+          // Only draw the elevator shaft line if this section has
+          // been revealed.
+          if (this.revealedMap[level][elevator - 1] === 1) {
+            graphics.rect(83 + (elevator - 1) * 16, 136 + level * 8, 2, 8, 0);
+          }
         }
       }
-    }
 
-    // Draw the elevator point.
-    graphics.rect(
-      83 + (elevator.getCurrentPosition().x - 1) * 16,
-      136 + Math.floor(elevator.getCurrentPosition().y / 53),
-      2,
-      3,
-      [0, 11, 12, 15, 1, 15, 12, 11][Math.floor((gameTime.getAFC() % 24) / 3)],
-    );
+      // Draw the elevator point.
+      graphics.rect(
+        83 + (elevator.getCurrentPosition().x - 1) * 16,
+        136 + Math.floor(elevator.getCurrentPosition().y / 53),
+        2,
+        3,
+        [0, 11, 12, 15, 1, 15, 12, 11][
+          Math.floor((gameTime.getAFC() % 24) / 3)
+        ],
+      );
+    }
   }
 }
 
